89c3a2344c9fe3d6256301d73bf57b603d312298
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-06 13:31:14 +0200

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.mnist',
-    version='1.0.1',
+    version='1.0.2a0',
     description='MNIST Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.mnist',
     license='GPLv3',
